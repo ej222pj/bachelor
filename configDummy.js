6dfbcdfa7aba5ff43c8f85609b106b608460044e
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2017-03-24 00:58:17 +0100

    added config dummy

---
+++ configDummy.js
@@ -0,0 +1,13 @@
+sessionSecret = "";
+consumer_key = '';
+consumer_secret = '';
+access_token_key = '';
+access_token_secret = '';
+
+module.exports = {
+    sessionSecret,
+    consumer_key,
+    consumer_secret,
+    access_token_key,
+    access_token_secret,
+};
