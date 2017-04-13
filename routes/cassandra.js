e0c2e812ac546c45666dcd9c8f24c0281ec55042
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2017-04-13 08:15:56 +0200

    Cassandra exp is setup

--- routes/cassandra.js
+++ routes/cassandra.js
@@ -7,7 +7,8 @@ const cassandra = require("cassandra-driver");
 var connection;
 var db_config = {
     contactPoints: ['127.0.0.1'],
-    keyspace: 'tweets'
+    keyspace: 'tweets',
+    setFetchSize: 10000
 };
 connection = new cassandra.Client(db_config);
 connection.connect(function (err, result) {
@@ -43,7 +44,7 @@ function RunTenTimes(run, start, end) {
                     throw err;
                 }
                 else {
-                    
+
                 }
             });
         }
@@ -58,23 +59,23 @@ function RunHundredTimes(run, start, end) {
         for (i = start; i < end; i++) {
             var query = 'INSERT INTO Tweets_fact (id, created_at, description, favourite_count, geo, in_reply_to_screen_name, in_reply_to_status_id, in_reply_to_status_id_str, lang, location, place, retweet_count, screen_name, source, text, time_zone, user_created_at, utc_offset) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
             var params = [run + end + i,
-                tweets[i].tweet.created_at,
-                tweets[i].tweet.user.description,
-                tweets[i].tweet.favorite_count,
+            tweets[i].tweet.created_at,
+            tweets[i].tweet.user.description,
+            tweets[i].tweet.favorite_count,
                 null,
-                tweets[i].tweet.in_reply_to_screen_name,
-                tweets[i].tweet.in_reply_to_status_id,
-                tweets[i].tweet.in_reply_to_status_id_str,
-                tweets[i].tweet.lang,
-                tweets[i].tweet.user.location,
+            tweets[i].tweet.in_reply_to_screen_name,
+            tweets[i].tweet.in_reply_to_status_id,
+            tweets[i].tweet.in_reply_to_status_id_str,
+            tweets[i].tweet.lang,
+            tweets[i].tweet.user.location,
                 null,
-                tweets[i].tweet.retweet_count,
-                tweets[i].tweet.user.screen_name,
-                tweets[i].tweet.source,
-                tweets[i].tweet.text,
-                tweets[i].tweet.user.time_zone,
-                tweets[i].tweet.user.created_at,
-                tweets[i].tweet.user.utc_offset,
+            tweets[i].tweet.retweet_count,
+            tweets[i].tweet.user.screen_name,
+            tweets[i].tweet.source,
+            tweets[i].tweet.text,
+            tweets[i].tweet.user.time_zone,
+            tweets[i].tweet.user.created_at,
+            tweets[i].tweet.user.utc_offset,
             ];
             connection.execute(query, params, { prepare: true }, function (err) {
                 if (err) {
@@ -87,6 +88,24 @@ function RunHundredTimes(run, start, end) {
         var endTime = new Date();
         console.log("Run " + run + " was done in " + (endTime - startTime) + " milliseconds")
     });
+}
+
+function readFromCassandra(run, limit) {
+    var startTime = new Date();
+    connection.stream("SELECT * FROM Tweets_fact LIMIT " + limit)
+        .on('readable', function () {
+            // readable is emitted as soon a row is received and parsed
+            var row;
+            while (row = this.read()) {
+                // process row
+            }
+        })
+        .on('end', function () {
+            // emitted when all rows have been retrieved and read
+            var endTime = new Date();
+            console.log("Run " + run + " was done in " + (endTime - startTime) + " milliseconds")
+        });
+
 }
 
 var firstMethod = function () {
@@ -186,14 +205,40 @@ var fourteenthMethod = function (someStuff) {
     return promise;
 };
 
+var readOneKMethod = function (someStuff) {
+    var promise = new Promise(function (resolve, reject) {
+        readFromCassandra(1, 1000)
+    });
+    return readFiveKMethod();
+};
+
+var readFiveKMethod = function (someStuff) {
+    var promise = new Promise(function (resolve, reject) {
+        readFromCassandra(2, 5000)
+    });
+    return readTenKMethod();
+};
+
+var readTenKMethod = function (someStuff) {
+    var promise = new Promise(function (resolve, reject) {
+        readFromCassandra(3, 10000)
+    });
+    return promise;
+};
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     firstMethod()
     res.send("Cassandra");
 });
 
+router.get('/read', function (req, res, next) {
+    readOneKMethod()
+    res.send("Read From Cassandra");
+});
+
 router.get('/insert', function (req, res, next) {
-     tweet.findTenK().then(function (tweets) {
+    tweet.findTenK().then(function (tweets) {
         var startTime = new Date();
         for (i = 0; i < 10000; i++) {
             var query = 'INSERT INTO Tweets_fact (id, created_at, description, favourite_count, geo, in_reply_to_screen_name, in_reply_to_status_id, in_reply_to_status_id_str, lang, location, place, retweet_count, screen_name, source, text, time_zone, user_created_at, utc_offset) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
